421d1af06c4a1799662ed56e34b5db7b122a1685
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-06-22 00:19:03 -0300

    Add react-router-dom and implement routing for Home page

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
+import "@fontsource-variable/open-sans/wdth-italic.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import "./globals.css";
 
 const root = document.querySelector("#root") as unknown as Element;
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
