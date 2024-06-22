86fb7064802d836f32d09b15bbffa52ddb17bd64
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-06-22 16:31:11 -0300

    Add firebase utility functions for authentication

---
+++ src/utils/firebase/firebase.utils.ts
@@ -0,0 +1,47 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider, type User as UserAuth } from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDvgEKxTTGOf42T_nqMsad_Hiwih3hWiHc",
+  authDomain: "e-commerce-ztm-react.firebaseapp.com",
+  projectId: "e-commerce-ztm-react",
+  storageBucket: "e-commerce-ztm-react.appspot.com",
+  messagingSenderId: "405680499412",
+  appId: "1:405680499412:web:8e4212c235e09021b32941",
+};
+
+export const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const database = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth: UserAuth) => {
+  const userDocumentReference = doc(database, "users", userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocumentReference);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await setDoc(userDocumentReference, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      const _error = error as Error;
+      console.error("error creating the user", _error.message);
+    }
+  }
+
+  return userDocumentReference;
+};
