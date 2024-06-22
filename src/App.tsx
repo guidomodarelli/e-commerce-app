ff7df21e9f8fe888fca8d8d847768f97b3f510e4
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-06-21 23:54:10 -0300

    Add Open Sans Variable font support in the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import Categories from "./components/Categories/Categories.component";
+// Supports weights 300-800
+import "@fontsource-variable/open-sans/wdth-italic.css";
 
 function App() {
   return <Categories />;
