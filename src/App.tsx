f3069a92fcb03b7b02e2936250d5360c36614363
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-06-22 01:23:28 -0300

    Add Header component to route in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import Header from "./components/Header/Header.component";
 import Home from "./pages/Home.page";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Header />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 }
