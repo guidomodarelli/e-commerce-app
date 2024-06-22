31b8811ce97e1bdd1cfdf1c77f52f822cafca454
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-06-22 01:23:15 -0300

    Add Logo component and Navigation in Header component

---
+++ src/components/Header/Navigation/Navigation.component.tsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+import styles from "./Navigation.module.css";
+import Logo from "../../../assets/Logo.tsx";
+
+interface NavigationProps {}
+
+function Navigation({}: NavigationProps) {
+  return (
+    <nav className={styles.navigation}>
+      <Link className={styles["logo-container"]} to={"/"}>
+        <Logo />
+      </Link>
+      <ul className={styles["nav-links-container"]}>
+        <li>
+          <Link className={styles["nav-link"]} to={"/shop"}>
+            SHOP
+          </Link>
+        </li>
+        <li>
+          <Link className={styles["nav-link"]} to={"/cart"}>
+            CART
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
