c02e67d2bb15e00e79e8bdcc9204bfcce00f7ab4
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-06-22 00:12:20 -0300

    Refactor Categories component to use custom hook for categories

--- src/components/Categories/Categories.component.tsx
+++ src/components/Categories/Categories.component.tsx
@@ -1,38 +1,11 @@
-import { uniqId } from "../../utils/id";
 import CategoryItem from "./CategoryItem/CategoryItem.component";
-import { Category } from "./category.types";
 import "./Categories.styles.css";
+import useCategories from "./hooks/useCategories.hook";
 
 interface CategoriesProps {}
 
 function Categories({}: CategoriesProps) {
-  const categories: Category[] = [
-    {
-      id: uniqId(),
-      title: "Hats",
-      img: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: uniqId(),
-      title: "Jackets",
-      img: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: uniqId(),
-      title: "Sneakers",
-      img: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: uniqId(),
-      title: "Women",
-      img: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: uniqId(),
-      title: "Men",
-      img: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
+  const { categories } = useCategories();
 
   return (
     <div className="categories-container">
