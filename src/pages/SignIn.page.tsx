02d61e73dfde48084d1d8137680af1db1640f256
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-06-22 16:31:23 -0300

    Add Google sign-in functionality to SignIn page

--- src/pages/SignIn.page.tsx
+++ src/pages/SignIn.page.tsx
@@ -1,7 +1,16 @@
+import { signInWithGooglePopup, createUserDocumentFromAuth } from "../utils/firebase/firebase.utils";
+
+const logGoogleUser = async () => {
+  const { user } = await signInWithGooglePopup();
+  const userDocumentReference = await createUserDocumentFromAuth(user);
+  console.log(userDocumentReference);
+};
+
 function SignIn() {
   return (
     <div>
       <h1>Sign In Page</h1>
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
     </div>
   );
 }
