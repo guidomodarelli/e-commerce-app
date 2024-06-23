063d9f0a3dbbe52bab050f2bcc157b411e681a4e
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-06-23 14:47:30 -0300

    Update import paths using aliases in tsconfig.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Header from "./components/Header/Header.component";
+import Header from "@components/Header/Header.component";
 import Home from "./pages/Home.page";
 import { Routes, Route } from "react-router-dom";
 import SignIn from "./pages/SignIn.page";
--- src/components/Header/Navigation/Navigation.component.tsx
+++ src/components/Header/Navigation/Navigation.component.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import styles from "./Navigation.module.css";
-import Logo from "../../../assets/Logo.tsx";
+import Logo from "@assets/Logo.tsx";
 
 interface NavigationProps {}
 
