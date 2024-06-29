6c64c349f1c938ea47c433e2908eceb43b167af3
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-06-29 19:10:42 -0300

    Add heading components and update layout styles

---
+++ src/components/Heading/H1.component.tsx
@@ -0,0 +1,9 @@
+import { PropsWithChildren, HTMLAttributes } from "react";
+
+interface H1Props extends PropsWithChildren, HTMLAttributes<HTMLElement> {}
+
+function H1({ children }: H1Props) {
+  return <h1 className="text-4xl mb-8">{children}</h1>;
+}
+
+export default H1;
--- src/pages/SignIn.page.tsx
+++ src/pages/SignIn.page.tsx
@@ -1,17 +1,40 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "@utils/firebase/firebase.utils";
+import Button from "@/components/Button/Button.component";
+import FormInput from "@/components/Form/FormInput.component";
+import H1 from "@/components/Heading/H1.component";
+import H2 from "@/components/Heading/H2.component";
+import Layout from "@/layouts/Layout";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+// import { signInWithGooglePopup, createUserDocumentFromAuth } from "@utils/firebase/firebase.utils";
 
-const logGoogleUser = async () => {
-  const { user } = await signInWithGooglePopup();
-  const userDocumentReference = await createUserDocumentFromAuth(user);
-  console.log(userDocumentReference);
-};
+// const logGoogleUser = async () => {
+//   const { user } = await signInWithGooglePopup();
+//   const userDocumentReference = await createUserDocumentFromAuth(user);
+//   console.log(userDocumentReference);
+// };
 
 function SignIn() {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit: SubmitHandler<FieldValues> = (data: FieldValues) => {
+    console.log(data);
+  };
+
   return (
-    <div>
-      <h1>Sign In Page</h1>
-      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-    </div>
+    <Layout>
+      <H1 className="text-4xl">Sign In Page</H1>
+      <div className="max-w-96 mx-auto">
+        <H2 className="text-3xl">Already have an account?</H2>
+        <span>Sign in with your email and password</span>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormInput label="Email" register={register("email")} />
+          <FormInput label="Password" register={register("password")} />
+          <div className="flex gap-2">
+            <Button type="submit">Sign In</Button>
+            <Button variant="google">Sign In with Google</Button>
+          </div>
+        </form>
+      </div>
+    </Layout>
   );
 }
 
