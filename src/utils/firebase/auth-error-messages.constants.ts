44ab9981a11a9cc586afa474d5f1b825fd870f14
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-01 16:55:35 -0300

    Update Firebase error code constants and messages

---
+++ src/utils/firebase/auth-error-messages.constants.ts
@@ -0,0 +1,4 @@
+export const FIREBASE_MESSAGE_WEAK_PASSWORD = "The password provided is too weak";
+export const FIREBASE_MESSAGE_EMAIL_EXISTS = "The account already exists for this email";
+export const FIREBASE_MESSAGE_INVALID_LOGIN_CREDENTIALS =
+  "Incorrect email or password. Please try again with different credentials";
