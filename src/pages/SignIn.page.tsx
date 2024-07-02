a5d54b6774443c95ddb6e5776f4c12e69fb2d1bf
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-02 12:23:12 -0300

    Update sign-in and sign-up pages with UserNotAuthenticated component

--- src/pages/SignIn.page.tsx
+++ src/pages/SignIn.page.tsx
@@ -1,13 +1,16 @@
 import SignInForm from "@/components/Authentication/SignIn/SignInForm.component";
+import UserNotAuthenticated from "@/components/Authentication/UserNotAuthenticated/UserNotAuthenticated.component";
 import H1 from "@/components/Heading/H1.component";
 import Layout from "@/layouts/Layout";
 
 function SignIn() {
   return (
-    <Layout>
-      <H1>Sign In</H1>
-      <SignInForm />
-    </Layout>
+    <UserNotAuthenticated>
+      <Layout>
+        <H1>Sign In</H1>
+        <SignInForm />
+      </Layout>
+    </UserNotAuthenticated>
   );
 }
 
--- src/pages/SignUp.page.tsx
+++ src/pages/SignUp.page.tsx
@@ -1,13 +1,16 @@
 import SignUpForm from "@/components/Authentication/SignUp/SignUpForm.component";
+import UserNotAuthenticated from "@/components/Authentication/UserNotAuthenticated/UserNotAuthenticated.component";
 import H1 from "@/components/Heading/H1.component";
 import Layout from "@/layouts/Layout";
 
 function SignUp() {
   return (
-    <Layout>
-      <H1>Create new account</H1>
-      <SignUpForm />
-    </Layout>
+    <UserNotAuthenticated>
+      <Layout>
+        <H1>Create new account</H1>
+        <SignUpForm />
+      </Layout>
+    </UserNotAuthenticated>
   );
 }
 
