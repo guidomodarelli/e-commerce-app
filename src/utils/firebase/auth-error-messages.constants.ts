4333b125007674844940c41874e616b3d43c85f7
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-01 22:05:09 -0300

    Add constant for "too many attempts" error message

--- src/utils/firebase/auth-error-messages.constants.ts
+++ src/utils/firebase/auth-error-messages.constants.ts
@@ -2,3 +2,5 @@ export const MESSAGE_WEAK_PASSWORD = "The password provided is too weak";
 export const MESSAGE_EMAIL_EXISTS = "The account already exists for this email";
 export const MESSAGE_INVALID_LOGIN_CREDENTIALS =
   "Incorrect email or password. Please try again with different credentials";
+export const MESSAGE_TOO_MANY_ATTEMPTS_TRY_LATER =
+  "Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later.";
