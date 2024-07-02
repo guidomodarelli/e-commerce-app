d9a536e90db59e8fbcdb679e936a31d69d32da0b
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-01 21:41:05 -0300

    Add loading functionality to Button component

--- src/components/Button/Button.component.tsx
+++ src/components/Button/Button.component.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@/utils/cn";
 import { ButtonHTMLAttributes, PropsWithChildren } from "react";
 import "./Button.styles.css";
+import LdsRing from "../Loaders/lds-ring/LsdRing.component";
 
 const BUTTON_VARIANT = {
   google: "google-sign-in",
@@ -9,12 +10,13 @@ const BUTTON_VARIANT = {
 
 interface ButtonProps extends PropsWithChildren, ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: keyof typeof BUTTON_VARIANT;
+  loading?: boolean;
 }
 
-function Button({ children, variant, ...otherProps }: ButtonProps) {
+function Button({ children, variant, loading = false, ...otherProps }: ButtonProps) {
   return (
-    <button className={cn("button-container", variant && BUTTON_VARIANT[variant])} {...otherProps}>
-      {children}
+    <button className={cn("button-container", variant ? BUTTON_VARIANT[variant] : "")} {...otherProps}>
+      {loading ? <LdsRing /> : children}
     </button>
   );
 }
