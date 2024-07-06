4c94ea15860a321ac00c7f97c03ab208d17cd028
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-06 12:14:28 -0300

    Refactor layout to remove unnecessary padding

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -3,7 +3,7 @@ import { PropsWithChildren } from "react";
 interface LayoutProps extends PropsWithChildren {}
 
 function Layout({ children }: LayoutProps) {
-  return <main className="mx-auto sm:max-w-screen-xl px-6 pb-16">{children}</main>;
+  return <main className="mx-auto sm:max-w-screen-xl pb-16">{children}</main>;
 }
 
 export default Layout;
