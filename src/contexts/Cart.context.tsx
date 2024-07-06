fc9fb1b5622ef4a19afa34dbe32be8650122549f
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-06 16:10:26 -0300

    Add function to add item to cart in CartContext

--- src/contexts/Cart.context.tsx
+++ src/contexts/Cart.context.tsx
@@ -1,3 +1,5 @@
+import { CartItem } from "@/global/types/cart-item.types";
+import { Product } from "@/global/types/products.types";
 import { Dispatch, PropsWithChildren, SetStateAction, createContext, useState } from "react";
 
 interface CartContextType {
@@ -5,6 +7,8 @@ interface CartContextType {
   setIsCartOpen: Dispatch<SetStateAction<boolean>>;
   openCart: () => void;
   closeCart: () => void;
+  cartItems: CartItem[];
+  addItemToCart: (newItem: Product) => void;
 }
 
 export const CartContext = createContext<CartContextType>({
@@ -12,21 +16,51 @@ export const CartContext = createContext<CartContextType>({
   setIsCartOpen: () => false,
   openCart: () => {},
   closeCart: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
 });
 
 interface CartProviderProps extends PropsWithChildren {}
 
 function CartProvider({ children }: CartProviderProps) {
   const [isCartOpen, setIsCartOpen] = useState<boolean>(false);
-  const value = {
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  const addCartItem = (cartItems: CartItem[], productToAdd: Product): CartItem[] => {
+    const cartItemIndex = cartItems.findIndex((item) => item.id === productToAdd.id);
+
+    if (cartItemIndex !== -1) {
+      const cartItem = cartItems[cartItemIndex];
+
+      cartItems[cartItemIndex] = {
+        ...cartItem,
+        quantity: cartItem.quantity + 1,
+      };
+      return [...cartItems];
+    }
+
+    return [
+      ...cartItems,
+      {
+        ...productToAdd,
+        quantity: 1,
+      },
+    ];
+  };
+
+  const value: CartContextType = {
     isCartOpen,
-    openCart: () => {
+    openCart() {
       setIsCartOpen(true);
     },
-    closeCart: () => {
+    closeCart() {
       setIsCartOpen(false);
     },
     setIsCartOpen,
+    cartItems,
+    addItemToCart(productToAdd: Product) {
+      setCartItems(addCartItem(cartItems, productToAdd));
+    },
   };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
