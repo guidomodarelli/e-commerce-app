86ba32e0bec1b98d2c22a09198d97008257447f3
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-06 19:28:22 -0300

    Update Header component styles and add Header.module.css

--- src/components/Header/Header.component.tsx
+++ src/components/Header/Header.component.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Navigation from "./Navigation/Navigation.component";
+import styles from "./Header.module.css";
 
 interface HeaderProps {}
 
 function Header({}: HeaderProps) {
   return (
     <>
-      <header>
-        <Navigation />
+      <header className={styles.header}>
+        <div className="max-w-7xl mx-auto flex items-center justify-between gap-16">
+          <Navigation />
+        </div>
       </header>
       <Outlet />
     </>
