6f010883d318d1695e7c94a20219455423374d34
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-07 18:15:59 -0300

    Update H1 component with select-none class

--- src/components/Heading/H1.component.tsx
+++ src/components/Heading/H1.component.tsx
@@ -3,7 +3,7 @@ import { PropsWithChildren, HTMLAttributes } from "react";
 interface H1Props extends PropsWithChildren, HTMLAttributes<HTMLElement> {}
 
 function H1({ children }: H1Props) {
-  return <h1 className="text-4xl mb-8">{children}</h1>;
+  return <h1 className="text-4xl mb-8 select-none">{children}</h1>;
 }
 
 export default H1;
