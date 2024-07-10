ec30866f195f15c4d67e9e0666aed1376072f165
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-10 12:41:43 -0300

    Refactor useProducts hook to return useContext directly

--- src/global/hooks/useProducts.hook.ts
+++ src/global/hooks/useProducts.hook.ts
@@ -2,11 +2,7 @@ import { CategoriesContext } from "@/contexts/Categories.context";
 import { useContext } from "react";
 
 function useProducts() {
-  const { categories } = useContext(CategoriesContext);
-
-  return {
-    categories,
-  };
+  return useContext(CategoriesContext);
 }
 
 export default useProducts;
