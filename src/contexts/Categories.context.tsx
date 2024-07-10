e27502c811c96ff98cffd1c7a9bcaec7b7bd2b4e
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-10 12:08:19 -0300

    Add CategoriesContext, remove ProductsContext

---
+++ src/contexts/Categories.context.tsx
@@ -0,0 +1,30 @@
+import { PropsWithChildren, createContext, useEffect, useState } from "react";
+import { getCategoriesAndDocuments } from "@/utils/firebase/firebase.utils";
+import { Product } from "@/global/types/products.types";
+
+interface CategoriesContextType {
+  categories: Record<string, Product[]>;
+}
+
+export const CategoriesContext = createContext<CategoriesContextType>({
+  categories: {},
+});
+
+interface CategoriesProviderProps extends PropsWithChildren {}
+
+export const CategoriesProvider = ({ children }: CategoriesProviderProps) => {
+  const [categories, setCategories] = useState<Record<string, Product[]>>({});
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      setCategories(categoryMap);
+    };
+
+    void getCategoriesMap();
+  });
+
+  const value = { categories };
+
+  return <CategoriesContext.Provider value={value}>{children}</CategoriesContext.Provider>;
+};
