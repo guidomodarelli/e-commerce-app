87b9ee60a83c20699bb523090ec0a08e88c2b7aa
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-20 11:58:48 -0300

    Update use case function names for consistency

--- src/core/domain/useCases/CreateAuthUserWithEmailAndPasswordUseCase.ts
+++ src/core/domain/useCases/CreateAuthUserWithEmailAndPasswordUseCase.ts
@@ -1,6 +1,6 @@
 import { UserAuthWithEmailAndPassword } from "@/core/ports/UserAuthWithEmailAndPassword.port";
 
-export const createAuthUserWithEmailAndPassword =
+export const createAuthUserWithEmailAndPasswordUseCase =
   (userAuth: UserAuthWithEmailAndPassword) => (email: string, password: string) => {
     return userAuth.signUp(email, password);
   };
--- src/core/domain/useCases/SignInAuthUserWithEmailAndPasswordUseCase.ts
+++ src/core/domain/useCases/SignInAuthUserWithEmailAndPasswordUseCase.ts
@@ -1,6 +1,6 @@
 import { UserAuthWithEmailAndPassword } from "@/core/ports/UserAuthWithEmailAndPassword.port";
 
-export const signInAuthUserWithEmailAndPassword =
+export const signInAuthUserWithEmailAndPasswordUseCase =
   (userAuth: UserAuthWithEmailAndPassword) => (email: string, password: string) => {
     return userAuth.signIn(email, password);
   };
