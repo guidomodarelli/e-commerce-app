c3994468ac647146a79471d588f13ebdaab4a741
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-21 15:02:48 -0300

    Add firebase and turso config objects to setup file

---
+++ src/config/index.ts
@@ -0,0 +1,13 @@
+export const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY as string,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN as string,
+  projectId: import.meta.env.VITE_PROJECT_ID as string,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET as string,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID as string,
+  appId: import.meta.env.VITE_APP_ID as string,
+};
+
+export const tursoConfig = {
+  url: import.meta.env.VITE_TURSO_DATABASE_URL as string,
+  authToken: import.meta.env.VITE_TURSO_AUTH_TOKEN as string,
+};
--- src/setup.ts
+++ src/setup.ts
@@ -23,22 +23,9 @@ import {
 } from "@core/domain/useCases";
 import { firebaseConfig, tursoConfig } from "./config";
 
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY as string,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN as string,
-  projectId: import.meta.env.VITE_PROJECT_ID as string,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET as string,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID as string,
-  appId: import.meta.env.VITE_APP_ID as string,
-};
 initializeApp(firebaseConfig);
 const auth = getAuth();
-
-const turso = createClient({
-  url: import.meta.env.VITE_TURSO_DATABASE_URL as string,
-  authToken: import.meta.env.VITE_TURSO_AUTH_TOKEN as string,
-});
-
+const turso = createClient(tursoConfig);
 const db = drizzle(turso);
 
 const userAuthWithEmailAndPassword = new UserAuthWithEmailAndPasswordFirebaseAdapter(auth);
