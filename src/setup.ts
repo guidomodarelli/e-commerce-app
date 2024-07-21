ef33859a417f61669b59faf084bb496bdc798dc0
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-21 15:03:04 -0300

    Add saveAllProductsUseCase function for product repository

--- src/setup.ts
+++ src/setup.ts
@@ -27,14 +27,14 @@ initializeApp(firebaseConfig);
 const auth = getAuth();
 const turso = createClient(tursoConfig);
 const db = drizzle(turso);
-
-const userAuthWithEmailAndPassword = new UserAuthWithEmailAndPasswordFirebaseAdapter(auth);
-const userRepository: UserRepository = new UserRepositoryDrizzleAdapter(db);
-
 const provider = new GoogleAuthProvider();
 provider.setCustomParameters({
   prompt: "select_account",
 });
+
+const userAuthWithEmailAndPassword = new UserAuthWithEmailAndPasswordFirebaseAdapter(auth);
+const userRepository: UserRepository = new UserRepositoryDrizzleAdapter(db);
+const productRepository: ProductRepository = new ProductRepositoryDrizzleAdapter(db);
 
 export const signInWithGoogle = () => signInWithPopup(auth, provider);
 export const onAuthStateChanged = (callback: NextOrObserver<User>) => onAuthStateChangedFirebase(auth, callback);
@@ -42,3 +42,4 @@ export const signInWithEmailAndPassword = signInAuthUserWithEmailAndPasswordUseC
 export const signUpWithEmailAndPassword = signUpAuthUserWithEmailAndPasswordUseCase(userAuthWithEmailAndPassword);
 export const signOut = signOutUserUseCase(userAuthWithEmailAndPassword);
 export const saveUser = SaveAuthUserUseCase(userRepository);
+export const saveAllProducts = saveAllProductsUseCase(productRepository);
