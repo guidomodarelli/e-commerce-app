4241d8b644dccd820b4c8795dba6d6c4dd9a0d4a
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-20 21:54:58 -0300

    Add UserRepositoryTursoAdapter for saving user to database

---
+++ src/core/adapters/UserRepositoryTursoAdapter.ts
@@ -0,0 +1,20 @@
+/* eslint-disable unicorn/prevent-abbreviations */
+import { Client } from "@libsql/client";
+import { User } from "../domain/entities/User";
+import { InfoExtra, UserRepository } from "../ports/UserRepository.port";
+
+export class UserRepositoryTursoAdapter implements UserRepository {
+  constructor(private readonly turso: Client) {}
+
+  async save(user: User, extra?: InfoExtra): Promise<void> {
+    const displayName = user.displayName ?? extra?.displayName ?? "";
+    try {
+      await this.turso.execute({
+        sql: "INSERT INTO users (email, displayName) VALUES (?, ?)",
+        args: [user.email, displayName],
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
