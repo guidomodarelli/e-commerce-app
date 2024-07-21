c4c4ef88c47dc93177ba70fab0fea338338dbadd
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-21 15:20:48 -0300

    Add Product import from core domain entities

--- src/components/product-card/ProductCard.component.tsx
+++ src/components/product-card/ProductCard.component.tsx
@@ -1,8 +1,8 @@
-import { Product } from "@global/types/products.types";
 import { toast } from "sonner";
 import Button from "../Button/Button.component";
 import "./product-card.styles.css";
 import { useCart } from "@global/contexts/Cart.context";
+import { Product } from "@core/domain/entities";
 
 interface ProductCardProps {
   product: Product;
