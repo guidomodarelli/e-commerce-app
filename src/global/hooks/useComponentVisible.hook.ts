401995971cffe1ee0c7c52498856fd3750b78ab2
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-21 15:15:30 -0300

    Refactor useComponentVisible hook with useCallback

--- src/global/hooks/useComponentVisible.hook.ts
+++ src/global/hooks/useComponentVisible.hook.ts
@@ -1,20 +1,23 @@
-import { useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef } from "react";
 
 export default function useClickOutside<T extends HTMLElement>(callback: () => void) {
   const ref = useRef<T>(null);
 
-  const handleClickOutside = (event: MouseEvent) => {
-    if (!ref.current?.contains(event.target as T)) {
-      callback();
-    }
-  };
+  const handleClickOutside = useCallback(
+    (event: MouseEvent) => {
+      if (!ref.current?.contains(event.target as T)) {
+        callback();
+      }
+    },
+    [callback],
+  );
 
   useEffect(() => {
     document.addEventListener("click", handleClickOutside, true);
     return () => {
       document.removeEventListener("click", handleClickOutside, true);
     };
-  }, []);
+  }, [handleClickOutside]);
 
   return { ref };
 }
