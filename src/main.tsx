cc475d12a09552dc7da5ab51ec33e4e5feda8c54
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-21 20:21:23 -0300

    Remove React.StrictMode for rendering optimization

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import "@fontsource-variable/open-sans/wdth-italic.css";
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "sonner";
@@ -10,12 +9,10 @@ import Providers from "./providers/Providers";
 const root = document.querySelector("#root") as unknown as Element;
 
 ReactDOM.createRoot(root).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Toaster closeButton richColors />
-      <Providers>
-        <App />
-      </Providers>
-    </BrowserRouter>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Toaster closeButton richColors />
+    <Providers>
+      <App />
+    </Providers>
+  </BrowserRouter>,
 );
