5be0b6496af6838d47b62c75221303b8f4664957
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-22 13:13:02 -0300

    Update schema import and db type in UserRepositoryDrizzleAdapter

--- src/core/adapters/drizzle/UserRepositoryDrizzleAdapter.ts
+++ src/core/adapters/drizzle/UserRepositoryDrizzleAdapter.ts
@@ -1,15 +1,15 @@
 import { LibSQLDatabase } from "drizzle-orm/libsql";
 import { User } from "../../domain/entities/User";
 import { InfoExtra, UserRepository } from "../../ports/UserRepository.port";
-import { userTable } from "./schemas/user";
+import * as schema from "./schema";
 
 export class UserRepositoryDrizzleAdapter implements UserRepository {
-  constructor(private readonly db: LibSQLDatabase) {}
+  constructor(private readonly db: LibSQLDatabase<typeof schema>) {}
 
   async save(user: User, extra?: InfoExtra): Promise<void> {
     const displayName = user.displayName ?? extra?.displayName ?? "";
     try {
-      await this.db.insert(userTable).values({
+      await this.db.insert(schema.users).values({
         email: user.email,
         displayName,
       });
