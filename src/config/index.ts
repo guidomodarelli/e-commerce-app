6f6aa3bc30a8aa805985acf2085ef76e5c9336de
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-22 20:40:12 -0300

    Update firebaseConfig keys to use VITE_FIREBASE prefix

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,12 +3,12 @@ import { validateEnvironment } from "./validate-envs";
 const env = validateEnvironment();
 
 export const firebaseConfig = {
-  apiKey: env.VITE_API_KEY,
-  authDomain: env.VITE_AUTH_DOMAIN,
-  projectId: env.VITE_PROJECT_ID,
-  storageBucket: env.VITE_STORAGE_BUCKET,
-  messagingSenderId: env.VITE_MESSAGING_SENDER_ID,
-  appId: env.VITE_APP_ID,
+  apiKey: env.VITE_FIREBASE_API_KEY,
+  authDomain: env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: env.VITE_FIREBASE_APP_ID,
 };
 
 export const tursoConfig = {
