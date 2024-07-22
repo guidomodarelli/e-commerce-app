9b21d55592ef31b3e48b8fdc5ab09d9406607923
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-22 20:30:17 -0300

    Validate environment variables before using in config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,15 +1,17 @@
-// TODO: delete .env and validate env vars with a schema
+import { validateEnvironment } from "./validate-envs";
+
+const env = validateEnvironment();
 
 export const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY as string,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN as string,
-  projectId: import.meta.env.VITE_PROJECT_ID as string,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET as string,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID as string,
-  appId: import.meta.env.VITE_APP_ID as string,
+  apiKey: env.VITE_API_KEY,
+  authDomain: env.VITE_AUTH_DOMAIN,
+  projectId: env.VITE_PROJECT_ID,
+  storageBucket: env.VITE_STORAGE_BUCKET,
+  messagingSenderId: env.VITE_MESSAGING_SENDER_ID,
+  appId: env.VITE_APP_ID,
 };
 
 export const tursoConfig = {
-  url: import.meta.env.VITE_TURSO_DATABASE_URL as string,
-  authToken: import.meta.env.VITE_TURSO_AUTH_TOKEN as string,
+  url: env.VITE_TURSO_DATABASE_URL,
+  authToken: env.VITE_TURSO_AUTH_TOKEN,
 };
---
+++ src/config/validate-envs.ts
@@ -0,0 +1,22 @@
+// validateEnv.ts
+import { z } from "zod";
+
+const envSchema = z.object({
+  VITE_API_KEY: z.string().min(1),
+  VITE_AUTH_DOMAIN: z.string().min(1),
+  VITE_PROJECT_ID: z.string().min(1),
+  VITE_STORAGE_BUCKET: z.string().min(1),
+  VITE_MESSAGING_SENDER_ID: z.string().min(1),
+  VITE_APP_ID: z.string().min(1),
+  VITE_TURSO_DATABASE_URL: z.string().url(),
+  VITE_TURSO_AUTH_TOKEN: z.string().min(1),
+});
+
+export const validateEnvironment = () => {
+  const parsedEnvironment = envSchema.safeParse(import.meta.env);
+  if (!parsedEnvironment.success) {
+    console.error("Invalid environment variables", parsedEnvironment.error.format());
+    throw new Error("Invalid environment variables");
+  }
+  return parsedEnvironment.data;
+};
