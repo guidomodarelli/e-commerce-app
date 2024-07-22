b333e716ed2d08099632da6c3e4eec183fa3743a
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-22 20:46:50 -0300

    Update config exports to use type annotations

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,10 @@
+import { Config } from "@libsql/client";
+import { FirebaseOptions } from "firebase/app";
 import { validateEnvironment } from "./validate-envs";
 
 const env = validateEnvironment();
 
-export const firebaseConfig = {
+export const firebaseConfig: FirebaseOptions = {
   apiKey: env.VITE_FIREBASE_API_KEY,
   authDomain: env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: env.VITE_FIREBASE_PROJECT_ID,
@@ -11,7 +13,7 @@ export const firebaseConfig = {
   appId: env.VITE_FIREBASE_APP_ID,
 };
 
-export const tursoConfig = {
+export const tursoConfig: Config = {
   url: env.VITE_TURSO_DATABASE_URL,
   authToken: env.VITE_TURSO_AUTH_TOKEN,
 };
