6298ac8407606c250b22202007ca68c9191371a4
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-22 21:36:36 -0300

    Refactor useCategories hook to fetch categories dynamically

--- src/hooks/useCategories.hook.ts
+++ src/hooks/useCategories.hook.ts
@@ -1,36 +1,22 @@
-import { Category } from "@core/domain/entities/Category";
-import { ulid } from "ulid";
+import { getCategories } from "@/setup";
+import { Category } from "@core/domain/entities";
+import { useState } from "react";
+import { useEffectOnce } from "react-use";
 
 function useCategories() {
-  const categories: Category[] = [
-    {
-      id: ulid(),
-      title: "Hats",
-      img: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: ulid(),
-      title: "Jackets",
-      img: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: ulid(),
-      title: "Sneakers",
-      img: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: ulid(),
-      title: "Women",
-      img: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: ulid(),
-      title: "Men",
-      img: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
+  const [categories, setCategories] = useState<Category[]>([]);
 
-  return { categories };
+  useEffectOnce(() => {
+    getCategories()
+      .then((categories_) => {
+        setCategories(categories_);
+      })
+      .catch(() => {});
+  });
+
+  return {
+    categories,
+  };
 }
 
 export default useCategories;
