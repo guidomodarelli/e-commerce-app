5e93b66780c3c1d30cdd765775790abeccb797a2
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-23 23:41:19 -0300

    Refactor useCategories to include fetched status

--- src/hooks/useCategories.hook.ts
+++ src/hooks/useCategories.hook.ts
@@ -3,17 +3,33 @@ import { Category } from "@core/domain/entities";
 import { useState } from "react";
 import { useEffectOnce } from "react-use";
 
-function useCategories() {
+interface CategoriesOutput {
+  categories: Category[];
+  hasBeenFetched: boolean;
+  getCategoryId: (categoryTitle?: Category["title"]) => Category["id"] | undefined;
+}
+
+function useCategories(): CategoriesOutput {
   const [categories, setCategories] = useState<Category[]>([]);
+  const [fetched, setFetched] = useState(false);
 
   useEffectOnce(() => {
     getCategories()
       .then(setCategories)
+      .then(() => {
+        setFetched(true);
+      })
       .catch(() => {});
   });
 
+  const getCategoryId = (categoryTitle?: Category["title"]) => {
+    return categories.find((category) => category.title === categoryTitle)?.id;
+  };
+
   return {
     categories,
+    hasBeenFetched: fetched,
+    getCategoryId,
   };
 }
 
