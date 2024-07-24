1a2381f7545f6820b5f28d55e4faafe365209b93
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-24 09:05:03 -0300

    Add Link component to CategoryPreview for dynamic routing

--- src/components/Categories/CategoriesPreview/CategoryPreview/CategoryPreview.component.tsx
+++ src/components/Categories/CategoriesPreview/CategoryPreview/CategoryPreview.component.tsx
@@ -1,16 +1,27 @@
 import { Product } from "@core/domain/entities";
 import "./category-preview.styles.css";
 import ProductCard from "@/components/product-card/ProductCard.component";
+import { Link } from "react-router-dom";
+import { cn } from "@utils/cn";
 
 interface CategoryPreviewProps {
   title: string;
   products: Product[];
+  center?: boolean;
 }
 
-function CategoryPreview({ title, products }: CategoryPreviewProps) {
+function CategoryPreview({ title, products, center }: CategoryPreviewProps) {
   return (
     <div className="category-preview-container">
-      <h2 className="title">{title.toUpperCase()}</h2>
+      <Link to={`/shop/${title.toLowerCase()}`}>
+        <h2
+          className={cn("title", {
+            "text-center": center,
+          })}
+        >
+          {title.toUpperCase()}
+        </h2>
+      </Link>
       <div className="preview">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
