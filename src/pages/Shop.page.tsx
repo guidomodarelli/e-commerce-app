f44b18d01d7c1869e82b1757615f621c9bfc0dc5
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-24 09:04:42 -0300

    Add Link component to wrap the Shop heading

--- src/pages/Shop.page.tsx
+++ src/pages/Shop.page.tsx
@@ -1,13 +1,16 @@
 import H1 from "@/components/Heading/H1.component";
 import Shop from "@/components/shop/Shop.component";
 import Layout from "@/layouts/Layout";
+import { Link } from "react-router-dom";
 
 interface ShopProps {}
 
 function ShopPage({}: ShopProps) {
   return (
     <Layout>
-      <H1>Shop</H1>
+      <Link to={"/shop"}>
+        <H1>Shop</H1>
+      </Link>
       <Shop />
     </Layout>
   );
