601380805d1f810dcc9be297591a6a709a331992
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-24 10:10:15 -0300

    Update CategoryPreview component to use module CSS for styling

--- src/components/Categories/CategoriesPreview/CategoryPreview/CategoryPreview.component.tsx
+++ src/components/Categories/CategoriesPreview/CategoryPreview/CategoryPreview.component.tsx
@@ -1,5 +1,5 @@
 import { Product } from "@core/domain/entities";
-import "./category-preview.styles.css";
+import styles from "./category-preview.module.css";
 import ProductCard from "@/components/product-card/ProductCard.component";
 import { Link } from "react-router-dom";
 import { cn } from "@utils/cn";
@@ -12,17 +12,17 @@ interface CategoryPreviewProps {
 
 function CategoryPreview({ title, products, center }: CategoryPreviewProps) {
   return (
-    <div className="category-preview-container">
+    <div className={styles["category-preview-container"]}>
       <Link to={`/shop/${title.toLowerCase()}`}>
         <h2
-          className={cn("title", {
+          className={cn(styles.title, {
             "text-center": center,
           })}
         >
           {title.toUpperCase()}
         </h2>
       </Link>
-      <div className="preview">
+      <div className={styles.preview}>
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
