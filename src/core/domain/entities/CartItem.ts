5a428759e74590f0b03b8d0614d92a5c5cf99399
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-26 00:35:26 -0300

    Refactor CartItem to class with quantity methods

--- src/core/domain/entities/CartItem.ts
+++ src/core/domain/entities/CartItem.ts
@@ -1,5 +1,27 @@
 import { Product } from "./Product";
 
-export interface CartItem extends Product {
-  quantity: number;
+export class CartItem implements Product {
+  id: string;
+  name: string;
+  imageUrl: string;
+  price: number;
+  categoryId?: string | undefined;
+  quantity = 1;
+
+  constructor(product: Product, quantity = 1) {
+    this.id = product.id;
+    this.name = product.name;
+    this.imageUrl = product.imageUrl;
+    this.price = product.price;
+    this.categoryId = product.categoryId;
+    this.quantity = quantity;
+  }
+
+  increment() {
+    this.quantity += 1;
+  }
+
+  decrement() {
+    this.quantity -= 1;
+  }
 }
