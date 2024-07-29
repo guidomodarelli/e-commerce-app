e465f9e270fe4dc62c61da4fb9d6e1f402dd5822
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-29 01:32:40 -0300

    Add reselect library for memoization in product selectors

--- src/store/products/products.selector.ts
+++ src/store/products/products.selector.ts
@@ -1,5 +1,10 @@
 import { useSelector } from "react-redux";
 import { AppRootState } from "../store";
 import { ProductState } from "./products.state";
+import { createSelector } from "reselect";
 
-export const useProductSelector = () => useSelector<AppRootState, ProductState>((state) => state.products);
+export const selectProducts = (state: AppRootState) => state.products;
+
+export const useProductSelector = () => useSelector<AppRootState, ProductState>(selectProducts);
+
+export const selectProductsList = createSelector([selectProducts], ({ list }) => list);
