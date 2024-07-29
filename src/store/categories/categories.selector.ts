7b27aad9266a40d7752b1f578c6de4dc1dc79247
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-07-29 01:33:08 -0300

    Refactor category selectors and add map selector

--- src/store/categories/categories.selector.ts
+++ src/store/categories/categories.selector.ts
@@ -1,14 +1,16 @@
-import { useSelector } from "react-redux";
 import { AppRootState } from "../store";
-import { CategoryState } from "./categories.state";
-import { Category } from "@core/domain/entities";
+import { Product } from "@core/domain/entities";
+import { createSelector } from "reselect";
+import { selectProductsList } from "@store/products";
 
-export const useCategorySelector = () => useSelector<AppRootState, CategoryState>((state) => state.categories);
+export const selectCategories = (state: AppRootState) => state.categories;
 
-export const getCategoryByTitleSelector = (list: Category[], categoryTitle: Category["title"]) => {
-  return list.find((category) => category.title === categoryTitle);
-};
+export const selectCategoriesList = createSelector([selectCategories], ({ list }) => list);
 
-export const getCategoryIdSelector = (list: Category[], categoryTitle: Category["title"]) => {
-  return getCategoryByTitleSelector(list, categoryTitle)?.id;
-};
+export const selectCategoriesMap = createSelector([selectCategoriesList, selectProductsList], (categories, products) =>
+  categories.reduce<Record<string, Product[]>>((previousValue, currentCategory) => {
+    const { id, title } = currentCategory;
+    previousValue[title.toLowerCase()] = products.filter((product) => product.categoryId === id);
+    return previousValue;
+  }, {}),
+);
