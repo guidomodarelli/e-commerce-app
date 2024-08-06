edfd2ef820eef4b8c8ca42d69dcd33029d685222
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-08-06 10:49:36 -0300

    Add isLoading state for products and categories in Category page

--- src/pages/Category.page.tsx
+++ src/pages/Category.page.tsx
@@ -3,10 +3,11 @@ import useCategories from "@/hooks/useCategories.hook";
 import CategoryPreview from "@components/Categories/CategoriesPreview/CategoryPreview/CategoryPreview.component";
 import { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import Spinner from "@components/spinner/Spinner.component";
 
 function Category() {
-  const { categoriesMap, hasBeenFetched, categoryExists } = useCategories();
-  useProducts();
+  const { categoriesMap, hasBeenFetched, categoryExists, isLoading: isLoadingCategories } = useCategories();
+  const { isLoading: isLoadingProducts } = useProducts();
   const { categoryTitle = "" } = useParams();
   const navigate = useNavigate();
 
@@ -15,6 +16,12 @@ function Category() {
       navigate("/404");
     }
   }, [hasBeenFetched, navigate, categoryTitle, categoryExists]);
+
+  const isLoading = isLoadingProducts || isLoadingCategories;
+
+  if (isLoading) {
+    return <Spinner />;
+  }
 
   return <CategoryPreview center title={categoryTitle} products={categoriesMap[categoryTitle]} />;
 }
