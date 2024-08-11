3e2865b1f1d40f208904685d5c9dc7e71758a17c
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-08-11 13:06:06 -0300

    Add composeEnhancer with eslint-disable directive

--- src/store/middleware/thunk.ts
+++ src/store/middleware/thunk.ts
@@ -1,6 +1,8 @@
 import { compose, MiddlewareAPI } from "redux";
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 export const composeEnhancer =
+  // @ts-expect-error window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ don't exists in window
   (!import.meta.env.PROD && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ?? compose;
 
 export const thunkMiddleware = (store: MiddlewareAPI) => (next: (action: unknown) => void) => (action: unknown) => {
