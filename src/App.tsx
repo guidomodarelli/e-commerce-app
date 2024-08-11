a5541ed2e27ebc8264aa1a6404150c875a1b7517
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-08-11 16:32:44 -0300

    Update code to lazy load components with Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import Header from "@components/Header/Header.component";
-import Home from "./pages/Home.page";
 import { Routes, Route } from "react-router-dom";
-import SignIn from "./pages/SignIn.page";
-import ShopPage from "./pages/Shop.page";
-import SignUp from "./pages/SignUp.page";
-import Page404 from "./pages/404.page";
-import CheckoutPage from "./pages/Checkout.page";
 import { useEffectOnce } from "react-use";
 import { checkUserSession } from "@store/user";
 import { useAppDispatch } from "@store/store";
+import { lazy, Suspense } from "react";
+import Spinner from "@components/spinner/Spinner.component";
+
+const Home = lazy(() => import("./pages/Home.page"));
+const SignIn = lazy(() => import("./pages/SignIn.page"));
+const ShopPage = lazy(() => import("./pages/Shop.page"));
+const SignUp = lazy(() => import("./pages/SignUp.page"));
+const Page404 = lazy(() => import("./pages/404.page"));
+const CheckoutPage = lazy(() => import("./pages/Checkout.page"));
 
 function App() {
   const dispatch = useAppDispatch();
@@ -18,16 +21,18 @@ function App() {
   });
 
   return (
-    <Routes>
-      <Route path="/" element={<Header />}>
-        <Route index element={<Home />} />
-        <Route path="shop/*" element={<ShopPage />} />
-        <Route path="signin" element={<SignIn />} />
-        <Route path="signup" element={<SignUp />} />
-        <Route path="checkout" element={<CheckoutPage />} />
-        <Route path="*" element={<Page404 />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route index element={<Home />} />
+          <Route path="shop/*" element={<ShopPage />} />
+          <Route path="signin" element={<SignIn />} />
+          <Route path="signup" element={<SignUp />} />
+          <Route path="checkout" element={<CheckoutPage />} />
+          <Route path="*" element={<Page404 />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
