f76e92ec94244e25fc80daba3fc5079fa142f8aa
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-08-26 00:23:52 -0300

    Update event identifiers to use full qualified names

--- src/core/common/Infrastructure/bus/ObserverEventBus.ts
+++ src/core/common/Infrastructure/bus/ObserverEventBus.ts
@@ -2,22 +2,22 @@ import { DomainEvent } from "@core/common/Domain";
 import { EventBus, Listener } from "@core/common/Domain/EventBus";
 
 export class ObserverEventBus implements EventBus {
-  private readonly subscribers = new Map<DomainEvent, Listener[]>();
+  private readonly subscribers = new Map<string, Listener[]>();
 
   subscribe(event: DomainEvent, newListener: Listener): () => void {
-    let listeners = this.subscribers.get(event) ?? [];
+    let listeners = this.subscribers.get(event.fullQualifiedEventName()) ?? [];
     listeners.push(newListener);
-    this.subscribers.set(event, listeners);
+    this.subscribers.set(event.fullQualifiedEventName(), listeners);
 
     return () => {
       listeners = listeners.filter((listener) => listener !== newListener);
-      this.subscribers.set(event, listeners);
+      this.subscribers.set(event.fullQualifiedEventName(), listeners);
     };
   }
 
   publish(...events: DomainEvent[]): void {
     for (const event of events) {
-      const listeners = this.subscribers.get(event);
+      const listeners = this.subscribers.get(event.fullQualifiedEventName());
       if (listeners && listeners.length > 0) {
         for (const listener of listeners) {
           listener(event);
