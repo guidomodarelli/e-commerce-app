de7473aff6e03652ffd44fd514b69d6ab91875c2
Guido Modarelli <guidomodarelli25@gmail.com>
2024-09-01 19:12:40 -0300

    Refactor DomainEvent interface to abstract class

--- src/core/common/Domain/DomainEvent.ts
+++ src/core/common/Domain/DomainEvent.ts
@@ -1,3 +1,38 @@
-export interface DomainEvent {
-  fullQualifiedEventName(): string;
+import { UUID } from "@core/Contexts/Shared/Domain/ValueObject/UUID";
+
+export abstract class DomainEvent {
+  static EVENT_NAME: string;
+  static fromPrimitives: (parameters: {
+    aggregateId: string;
+    eventId: string;
+    occurredOn: Date;
+    attributes: DomainEventAttributes;
+  }) => DomainEvent;
+
+  readonly aggregateId: string;
+  readonly eventId: string;
+  readonly occurredOn: Date;
+  readonly eventName: string;
+
+  constructor(parameters: { eventName: string; aggregateId: string; eventId?: string; occurredOn?: Date }) {
+    const { aggregateId, eventName, eventId, occurredOn } = parameters;
+    this.aggregateId = aggregateId;
+    this.eventId = eventId ?? UUID.random().value;
+    this.occurredOn = occurredOn ?? new Date();
+    this.eventName = eventName;
+  }
+
+  abstract toPrimitives(): DomainEventAttributes;
 }
+
+export interface DomainEventClass {
+  EVENT_NAME: string;
+  fromPrimitives(parameters: {
+    aggregateId: string;
+    eventId: string;
+    occurredOn: Date;
+    attributes: DomainEventAttributes;
+  }): DomainEvent;
+}
+
+type DomainEventAttributes = unknown;
