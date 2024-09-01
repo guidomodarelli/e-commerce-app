9ea9e24cbf6506fad2ffc69d3f462bfdce0ee2ac
Guido Modarelli <guidomodarelli25@gmail.com>
2024-09-01 19:16:43 -0300

    Update Actions type to only include UserAction

--- src/store/types.ts
+++ src/store/types.ts
@@ -1,6 +1,3 @@
-import { CartAction } from "./cart";
-import { CategoryAction } from "./categories";
-import { ProductAction } from "./products";
 import { UserAction } from "./user";
 
-export type Actions = ProductAction | CategoryAction | CartAction | UserAction;
+export type Actions = UserAction;
