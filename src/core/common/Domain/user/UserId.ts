59006ca9278a161b71a6fed85b20536fed9a5630
Guido Modarelli <guidomodarelli25@gmail.com>
2024-09-01 19:16:00 -0300

    Update UserId class to extend StringValueObject

--- src/core/common/Domain/user/UserId.ts
+++ src/core/common/Domain/user/UserId.ts
@@ -1,19 +1,3 @@
-import z from "zod";
-import { ValueObject } from "../ValueObject";
+import { StringValueObject } from "@core/Contexts/Shared/Domain/ValueObject/StringValueObject";
 
-export class UserId extends ValueObject<string> {
-  constructor(id: string) {
-    super(id);
-    if (!UserId.isValid(id)) {
-      throw new Error(UserId.invalidMessage(id));
-    }
-  }
-
-  public static isValid(id: string): boolean {
-    return z.string().safeParse(id).success;
-  }
-
-  public static invalidMessage(id: string): string {
-    return `The ID ${id} is not valid.`;
-  }
-}
+export class UserId extends StringValueObject {}
