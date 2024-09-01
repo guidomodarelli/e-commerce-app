9be86d5ad7bea6ef7b7c2337fe8b5078b52fb2ce
Guido Modarelli <guidomodarelli25@gmail.com>
2024-09-01 19:19:43 -0300

    refactor: Improve validation logic for UserDisplayName

--- src/core/common/Domain/user/UserDisplayName.ts
+++ src/core/common/Domain/user/UserDisplayName.ts
@@ -1,19 +1,22 @@
-import z from "zod";
-import { ValueObject } from "../ValueObject";
+import { StringValueObject } from "@core/Contexts/Shared/Domain/ValueObject/StringValueObject";
 
-export class UserDisplayName extends ValueObject<string> {
+export class UserDisplayName extends StringValueObject {
   public static MIN_LENGTH = 3;
   public static MAX_LENGTH = 100;
 
   constructor(displayName: string) {
     super(displayName);
+    this.ensureIsValidDisplayName(displayName);
+  }
+
+  private ensureIsValidDisplayName(displayName: string) {
     if (!UserDisplayName.isValid(displayName)) {
       throw new Error(UserDisplayName.invalidMessage(displayName));
     }
   }
 
   public static isValid(displayName: string): boolean {
-    return z.string().min(UserDisplayName.MIN_LENGTH).max(UserDisplayName.MAX_LENGTH).safeParse(displayName).success;
+    return displayName.length >= UserDisplayName.MIN_LENGTH && displayName.length <= UserDisplayName.MAX_LENGTH;
   }
 
   public static invalidMessage(displayName: string): string {
