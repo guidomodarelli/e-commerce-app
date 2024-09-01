c15ef35232b2635df542fcd6b06fa182d091eae7
Guido Modarelli <guidomodarelli25@gmail.com>
2024-09-01 19:16:50 -0300

    Add eslint-disable-next-line for @typescript-eslint/no-unused-vars

--- src/store/middleware/thunk.ts
+++ src/store/middleware/thunk.ts
@@ -5,6 +5,7 @@ export const composeEnhancer =
   // @ts-expect-error window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ don't exists in window
   (!import.meta.env.PROD && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ?? compose;
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const thunkMiddleware = (store: MiddlewareAPI) => (next: (action: unknown) => void) => (action: unknown) => {
   if (typeof action === "function") {
     action(/* dispatch */);
