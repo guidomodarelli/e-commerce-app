21e035175dacb5f3785f5aeb602e3250d0ee40c7
Guido Modarelli <guidomodarelli25@gmail.com>
2024-09-01 21:52:10 -0300

    feat: Add OmitStrict utility for UserAuth type

--- src/core/Contexts/Shop/Auth/Domain/UserAuth.ts
+++ src/core/Contexts/Shop/Auth/Domain/UserAuth.ts
@@ -1,3 +1,4 @@
+import { OmitStrict } from "@core/Contexts/Shared/Domain/OmitStrict";
 import { User } from "../../User/Domain/User";
 
-export type UserAuth = Pick<Omit<User, "id">, "email" | "displayName"> & { password: string };
+export type UserAuth = Pick<OmitStrict<User, "id">, "email" | "displayName"> & { password: string };
