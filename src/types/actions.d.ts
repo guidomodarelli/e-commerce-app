676db95749ca16fc7e6bea44a66e4ca71474e7a2
Guido Modarelli <guidomodarelli25@gmail.com>
2024-09-22 10:25:23 -0300

    Refactor action types for better readability and flexibility

--- src/types/actions.d.ts
+++ src/types/actions.d.ts
@@ -1,3 +1,5 @@
-type Action<T extends string, P = undefined> = {
-  type: T;
-} & (P extends undefined ? object : { payload: P });
+type Payload<T> = T extends void ? void : { payload: T };
+
+type ActionType<T extends string> = { type: T };
+
+type Action<T extends string, P = void> = ActionType<T> & Payload<P>;
